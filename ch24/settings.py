46904200d969b9343d39e2ccec304a17bb08dd2a
Jay Pound <jameshpoundiii@gmail.com>
2024-11-26 14:59:46 -0500

    ssl settings

--- ch24/settings.py
+++ ch24/settings.py
@@ -252,3 +252,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
